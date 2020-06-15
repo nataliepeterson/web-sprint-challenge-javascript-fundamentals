31df2d774d0c61fedf41424c9c6819cf2e10d164
Natalie Peterson <nataliealice.peterson@gmail.com>
2020-06-14 19:53:27 -0700

    arrays & callbacks

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,21 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+//This is due to scoping. the variable const internal is avaialbe to fucntion nestedFucntion() bc it is within that scope. It was declared inside myFunction().
+// It is all within myFuntion().
 
 /* Task 2: Counter */
 
-/* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+/* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. 
+For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+/*function summation(num){
+  for (let i=1; i<num; i++){
+    let add= 1
+    return num + add;
+  }
+}
+console.log(summation(4));
+
+
+  
